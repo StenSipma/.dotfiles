52996ad1a517b2a2073d72fd120a457e6fbf9c9c
Sten Sipma <sten.sipma@ziggo.nl>
2022-08-27 15:42:49 +0200

    Updates to install scripts

--- bin/install/programming.sh
+++ bin/install/programming.sh
@@ -2,6 +2,9 @@
 
 # Python
 sudo pacman -S --needed python-pip python-pipx
+# Needed to resolve issue with keys
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+paru -S --needed python39
 
 # Rust
 sudo pacman -S --needed rust
@@ -12,11 +15,26 @@ sudo pacman -S --needed go
 
 # Node (via NVM)
 paru -S --needed nvm
-if [[ -n "$NVM_DIR" ]]; then
+if [[ ! -n "$NVM_DIR" ]]; then
         source /usr/share/nvm/init-nvm.sh
         nvm install 16
+else
+        echo "nvm already installed. Skipping..."
 fi
 
 # Docker
 sudo pacman -S --needed docker
 sudo systemctl enable --now docker.service
+
+# Jupyter hub
+sudo pacman -S --needed jupyter-notebook
+paru -S --needed jupyter_contrib_nbextensions
+
+if [[ ! -d $(jupyter --data-dir)/nbextensions/vim_binding ]]; then
+        mkdir -p $(jupyter --data-dir)/nbextensions
+        cd $(jupyter --data-dir)/nbextensions
+        git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+        jupyter nbextension enable vim_binding/vim_binding
+else
+        echo "Vim bindings already installed"
+fi
