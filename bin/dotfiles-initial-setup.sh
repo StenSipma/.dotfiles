0845f61ed52a453d69bebbcb85595dddf3fbf341
Sten Sipma <sten.sipma@ziggo.nl>
2022-08-27 13:21:08 +0200

    Adjustmet to init script

--- bin/dotfiles-initial-setup.sh
+++ bin/dotfiles-initial-setup.sh
@@ -2,14 +2,15 @@
 
 # Make sure you are in `$HOME`
 # Clone repo to the machine
+mkdir -p $HOME/.dotfiles
 git clone --bare https://github.com/StenSipma/.dotfiles.git $HOME/.dotfiles
 
 # Create the alias used for managing the repo
 alias dgit='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # Make backup of existing files
-mkdir .dotfiles-backup
-dgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+mkdir -p $HOME/.dotfiles-backup
+dgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backup/{}
 
 # Copy files to local system
 dgit checkout
