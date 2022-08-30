7561993634b73529c29054210f4aa826b1700241
Sten Sipma <sten.sipma@ziggo.nl>
2022-08-30 08:41:45 +0200

    Added a pacman hook for XMonad recompile

--- bin/install/xmonad.sh
+++ bin/install/xmonad.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-echo "Installing Xmonad"
+echo "Adding XMonad recompile hook to pacman"
+if [[ ! -f /etc/pacman.d/hooks/xmonad.hook ]]; then
+        sudo mkdir -p /etc/pacman.d/hooks/
+        sudo ln -sf $XDG_CONFIG_HOME/pacman-hooks/xmonad.hook /etc/pacman.d/hooks/xmonad.hook
+else
+        echo "XMonad.hook already exists. Skipping..."
+fi
+
+echo "Installing XMonad"
 sudo pacman -S --needed xmonad xmonad-contrib
-xmonad --recompile
 
 echo "Installing used applications"
-sudo pacman -S --needed rofi kitty playerctl flameshot nitrogen picom stalonetray pasystray xmobar dunst dmenu xautolock adobe-source-code-pro-fonts noto-fonts noto-fonts-emoji xdotool
-paru -S --needed betterlockscreen otf-hasklig nerd-fonts-fira-code
+sudo pacman -S --needed \
+        rofi \
+        kitty \
+        playerctl \
+        flameshot \
+        nitrogen \
+        picom \
+        stalonetray \
+        pasystray \
+        xmobar \
+        peek \
+        dunst \
+        dmenu \
+        xautolock \
+        adobe-source-code-pro-fonts \
+        noto-fonts \
+        noto-fonts-emoji \
+        xdotool
 
+paru -S --needed betterlockscreen otf-hasklig nerd-fonts-fira-code
 mkdir -p $HOME/.wallpapers
+
