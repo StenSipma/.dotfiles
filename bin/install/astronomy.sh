c61f502fc094ebb41710d07855eab90e3b19c190
Sten Sipma <sten.sipma@ziggo.nl>
2023-06-17 14:06:43 +0200

    Add astronomy installation files

---
+++ bin/install/astronomy.sh
@@ -0,0 +1,10 @@
+paru -S --needed astromatic-sextractor
+paru -S --needed astromatic-swarp
+
+# Dependency for scamp
+sudo pacman -S --needed lapacke
+paru -S --needed astromatic-scamp
+
+# Add theli when it works (now in Project/Theli-install)
+# sudo pacman -S --needed libraw
+# paru -S --needed theli
