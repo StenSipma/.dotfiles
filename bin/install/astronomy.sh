32a81c2a53b0ac1bc7d2cb473567f6f3e7808f1f
Sten Sipma <sten.sipma@ziggo.nl>
2023-07-23 12:37:07 +0200

    Add Theli

--- bin/install/astronomy.sh
+++ bin/install/astronomy.sh
@@ -9,7 +9,7 @@ paru -S --needed astromatic-scamp
 
 # Add theli when it works (now in Project/Theli-install)
 # sudo pacman -S --needed libraw
-# paru -S --needed theli
+paru -S --needed theli
 
 paru -S --needed astrometry.net
 # TODO: Add index files as well
