814965d0a347fdad9e034f371979144dbbeb6cb1
Sten Sipma <sten.sipma@ziggo.nl>
2025-02-11 12:24:46 +0100

    .profile and xinitrc commands
    Added env variables for AtomDB (for pyatomdb)
    Added config stuff for HEA things

--- .config/xinitrc.d/Auriga-commands.sh
+++ .config/xinitrc.d/Auriga-commands.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+# For Optimus-Manager
+if [[ -f "/usr/bin/prime-offload" ]]; then
+    /usr/bin/prime-offload
+fi
 
 ## xrandr commands for correct display resolution, gpu driver etc.
-#xrandr --listproviders > providers-x.txt
-#xrandr --setprovideroutputsource modesetting NVIDIA-0
+# xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --auto
 # xrandr --dpi 331 # This makes the menu bars larger, not entirely sure why
 
 # TODO: change this to /etc/optimus-manager/xsetup-intel.sh or .../xsetup-nvidia.sh
-xrandr --output eDP-1 --mode "1920x1080"
+# xrandr --output eDP-1 --mode "1920x1080"
 # NOTE: without this, menu/dunst/dmenu are very large.
 xrandr --dpi 96
 autorandr --change
