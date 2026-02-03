792484b146056352b6c0b42e04250f0f2b439873
StenSipma <sten.sipma@ziggo.nl>
2026-02-03 11:58:48 +0100

    Properly pass on_attach & capabilities to the lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -107,8 +107,8 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
     vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', ']D', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
@@ -116,6 +116,7 @@ local on_attach = function(client, bufnr)
 end
 
 
+-- Default, config for all Lsp's if no other config is added
 vim.lsp.config("*", {
     on_attach = on_attach,
     capabilities = capabilities,
@@ -146,6 +147,8 @@ vim.lsp.config('pyright', {
         pyright = { disableOrganizeImports = true },
         defaultVenv = { '.venv' },
     },
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- Lua (lua_ls) – make Neovim runtime visible
@@ -166,6 +169,8 @@ vim.lsp.config('lua_ls', {
             telemetry = { enable = false },
         },
     },
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- -- Rust (rust_analyzer) – using rust-tools for extra UI
@@ -194,6 +199,8 @@ vim.lsp.config('texlab', {
             chktex = { onEdit = true },
         },
     },
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- Nix (nixd)
@@ -214,6 +221,8 @@ vim.lsp.config('nixd', {
             -- }
         },
     },
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 
